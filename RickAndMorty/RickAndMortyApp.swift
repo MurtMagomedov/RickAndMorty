f3f022e17f836bf1aa02b6d7cee64e2064685730
Murtazali <m.magomedov.a@mail.ru>
2025-01-10 13:37:46 +0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Муртазали Магомедов on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
